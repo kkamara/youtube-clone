c080768fd941560d6cf7f4b6ddf98496ffe2c3b9
kkamara <kelvinkamara@protonmail.com>
2022-09-12 06:52:01 +0100

    Parse html entities

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Box, CardContent, CardMedia, Typography, } from '@mui/material'
 import { CheckCircle, } from '@mui/icons-material'
 import { Link, } from 'react-router-dom'
+import he from 'he'
 
 import { demoProfilePicture, demoChannelTitle, } from '../utils/constants'
 
@@ -25,11 +26,11 @@ const ChannelCard = ({ channelDetail, marginTop, }) => {
         <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
           <CardMedia
             image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
-            alt={channelDetail?.snippet?.title}
+            alt={he.decode(channelDetail?.snippet?.title)}
             sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3', }}
           />
           <Typography variant='h6'>
-            {channelDetail?.snippet?.channelTitle || demoChannelTitle}
+            {(channelDetail?.snippet?.channelTitle && he.decode(channelDetail?.snippet?.channelTitle)) || demoChannelTitle}
             <CheckCircle sx={{ fontSize: 14, color: 'gray', m1: '5px', }} />
             {channelDetail?.statistics?.subscriberCount && (
               <Typography>
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, } from 'react-router-dom'
 import { Typography, Card, CardContent, CardMedia, } from '@mui/material'
 import { CheckCircle, } from '@mui/icons-material'
+import he from 'he'
 
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle, } from '../utils/constants'
 
@@ -10,20 +11,20 @@ const VideoCard = ({ video: { id: { videoId }, snippet, }, }) => {
     <Card sx={{ width: { xs: '100%', sm: '358px', md: '320px', }, boxShadow: 'none', borderRadius: 0, }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
-          image={snippet?.thumbnails?.high?.url}
-          alt={snippet?.title}
+          image={he.decode(snippet?.thumbnails?.high?.url)}
+          alt={he.decode(snippet?.title)}
           sx={{ width: { xs: '100%', sm: '358px', md: '320px', }, height: 180, }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px', }} >
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
           <Typography variant='subtitle1' fontWeight='bold' color='#fff'>
-            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+            {(snippet?.title && he.decode(snippet?.title.slice(0, 60))) || demoVideoTitle.slice(0, 60)}
           </Typography>
         </Link>
         <Link to={videoId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
           <Typography variant='subtitle2' fontWeight='bold' color='grey'>
-            {snippet?.channelTitle || demoChannelTitle}
+            {(snippet?.channelTitle && he.decode(snippet?.channelTitle)) || demoChannelTitle}
             <CheckCircle sx={{ fontSize: 12, color: 'gray', m1: '5px', }}/>
           </Typography>
         </Link>
